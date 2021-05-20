8800c1db2946d763d99c5f6acbac6a9a11baf922
joconstantine <joconstantine@users.noreply.github.com>
2021-05-20 23:25:52 +0800

    20/05/2021 23:25

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { spans: 0 };
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() { //after render
+        this.imageRef.current.addEventListener('load', this.setSpans); // to call setSpans after 'load'
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10);
+
+        // this.setState({ spans: spans}); 
+        this.setState({ spans }); // alternate syntax
+    }
+
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div>
-                <img alt={description}
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img ref={this.imageRef}
+                    alt={description}
                     src={urls.regular}
                 />
             </div>
