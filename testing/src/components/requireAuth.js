d9eca0a0b35ace1138e7dd1b1bd25b64f4734a35
Constantine Nguyen <constantine@ncs.com.sg>
2021-09-01 17:16:05 +0800

    01/09/2021 17:16

---
+++ testing/src/components/requireAuth.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const requireAuth = (ChildComponent) => {
+
+    class ComposedComponent extends React.Component {
+        // Our component just got rendered
+        componentDidMount() {
+            this.shouldNavigateAway();
+        }
+
+        // Our component just got updated
+        componentDidUpdate() {
+            this.shouldNavigateAway();
+        }
+
+        shouldNavigateAway() {
+            if (!this.props.auth) {
+                this.props.history.push('/');
+            }
+        }
+
+        render() {
+            return <ChildComponent {...this} />;
+        }
+    }
+
+    const mapStateToProps = ({ auth }) => {
+        return { auth };
+    };
+
+    return connect(mapStateToProps)(ComposedComponent);
+};
+
+export default requireAuth;
